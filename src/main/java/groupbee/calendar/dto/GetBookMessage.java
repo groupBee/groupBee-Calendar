aa0e2423fc61a1b506471bfba1cb11cacfc0257b
Jinwook Jung <jeenuk.chung@gmail.com>
2024-08-04 22:36:48 +0900

    Redis 예약 Pub Sub Controller

---
+++ src/main/java/groupbee/calendar/dto/GetBookMessage.java
@@ -0,0 +1,15 @@
+package groupbee.calendar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetBookMessage {
+    private String carID; // 차량 번호
+    private String carType; // 차량 종류
+    private String rentDay; // 대여시간
+    private String returnDay; //반납시간
+}
